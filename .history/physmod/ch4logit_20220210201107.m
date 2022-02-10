d8f7a4c23ff20984ef8cd15ae5361e0ba3b2b68b
Thanh Tran <tranquangthanh.ftu@gmail.com>
2022-02-10 20:17:05 +0900

    update code

---
+++ .history/physmod/ch4logit_20220210201107.m
@@ -0,0 +1,8 @@
+clearvars;
+n = 1000;
+r = 2.4;
+X(1) = 0.5;
+for i = 1:n 
+    X(i+1) = r*X(i)*(1-X(i));
+end 
+plot(X)
---
+++ .history/physmod/ch4logit_20220210201256.m
@@ -0,0 +1,12 @@
+clearvars;
+n = 100;
+r = 2.4;
+X(1) = 0.5;
+for i = 1:10
+    r = r+i/10;
+    for i = 1:n 
+        X(i+1) = r*X(i)*(1-X(i));
+    end 
+    plot(X)
+    hold on
+end 
