51875632e4c4c9a9fcc0bb8c3a837bf3d81a5092
Thanh Tran <tranquangthanh.ftu@gmail.com>
2022-02-11 17:56:44 +0900

    commit

--- .history/physmod/test_20220211155618.m
+++ .history/physmod/test_20220211155618.m
@@ -1,4 +1,4 @@
 clearvars;
 %A = fibonacci(10);
 %ch5fib_triple(10);
-%ezplot(@error_func, [-2,5]);
+fzero(@error_func, [2,4])
--- .history/physmod/test_20220211155745.m
+++ .history/physmod/test_20220211155745.m
@@ -1,4 +1,4 @@
 clearvars;
 %A = fibonacci(10);
 %ch5fib_triple(10);
-%ezplot(@error_func, [-2,5]);
+ezplot(fzero(@error_func, [-2,5]))
